48b8da0ab15f68c7a428bd2faf0370a43afd4ca1
nadiiamein <npodufala15@gmail.com>
2021-12-08 18:49:27 +0100

    add   InfoArtis

---
+++ src/components/InfoArtist.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="info-artist">
+        <div class="info-artist__name">{{name}}</div>
+                <div class="info-artist__summary" v-html="summary"/>
+                        <div class="info-artist__tags">
+                                    <div class="info-artist__tag"
+                                    v-vor="tag in tags"
+                                    :key="tags">
+                                    {{tag}}
+
+                                    </div>
+
+                        </div>
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        name: String,
+        summary: String,
+        tags: Array,
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.info-artist {
+    margin-top: 5rem;
+
+    &__name {
+        font-size: 2.4rem;
+    }
+
+    &__summary {
+        font-size: 1.4rem;
+        line-height: 1.5;
+        margin-top: 2rem;
+
+        ::v-deep(a) {
+            color: inherit;
+        }
+    }
+
+    &__tags {
+        display: flex;
+        gap: 1rem;
+        flex-wrap: wrap;
+        margin-top: 2rem;
+    }
+
+    &__tag {
+        font-size: 1.4rem;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        background: #e1e1e8;
+        border-radius: .5rem;
+        padding: .5rem 1rem;
+        user-select: none;
+    }
+}
+</style>
